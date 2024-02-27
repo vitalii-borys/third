2b06bc4946eee3c95375ce9d172620bfe1587c3f
vitalii-borys <61546859+vitalii-borys@users.noreply.github.com>
2024-02-26 23:21:09 -0600

    Just the be of 2024

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-document.body.innerHTML = '<h1>Javascript added</h1>'
+/* for (var i = 1; i < 3; i++)
+{console.log(i);}
+
+for (d = 0; d < 6; d++)
+{
+    if (d % 2 == 0)
+    {
+        continue;
+    }
+    console.log(d + " то є непарне число.")
+}
+
+var myArray = ["What is the meaning of life?", "The meaning of life is", 42];
+
+for (e = 0; e < myArray.length; e++)
+{
+    console.log(myArray[e]);
+} */
+
+/*function greet(name)
+{
+    return ("Привіт, " + name + " !")
+}
+console.log(greet("Bob"));p*/
+for (var i = 1; i < 31; i = i + 1)
+{console.log(i + "-й крок" + " i = " + i);}
+for (var a = 1; a < 31; a = Math.pow(2, a - 1))
+{console.log(a + "a");}
